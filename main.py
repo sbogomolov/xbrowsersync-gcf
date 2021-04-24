274085f0083bb1db6aa4518919129e790c2ca798
Sergii Bogomolov <sergii@bogomolov.io>
2021-04-24 23:50:28 +0200

    Add get_bookmarks_version

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from http import HTTPStatus
-from firebase_admin import firestore
+from firebase_admin import DocumentReference, firestore
 from flask import Response, Request
 from google.cloud import runtimeconfig
 from os import environ
@@ -22,6 +22,10 @@ db = firestore.client()
 def accept_new_syncs() -> bool:
     accept_new_syncs = config.get_variable("accept_new_syncs")
     return True if accept_new_syncs and accept_new_syncs.text.lower() == "true" else False
+
+
+def get_document(id_: str) -> DocumentReference:
+    return db.collection(COLLECTION).document(id_)
 
 
 def method_not_allowed(allowed_methods: List[str]) -> Response:
@@ -54,6 +58,25 @@ def bookmarks(request: Request):
         return bad_request('"version" is not provided')
 
     bookmarks = Bookmarks(version=version)
-    db.collection(COLLECTION).document(bookmarks.id_).set(bookmarks.to_dict())
+    get_document(bookmarks.id_).set(bookmarks.to_dict())
 
     return {"id": bookmarks.id_, "lastUpdated": bookmarks.last_updated_str, "version": bookmarks.version}
+
+
+def get_bookmarks_version(request: Request):
+    if request.method != "GET":
+        return method_not_allowed(["GET"])
+
+    request_json = request.get_json(silent=True)
+    if not request_json:
+        return bad_request("Request body is empty")
+
+    id_ = request_json.get("id")
+    if not id_:
+        return bad_request('"id" is not provided')
+
+    doc = get_document(id_).get()
+    if not doc.exists:
+        return bad_request(f'Bookmarks sync with id "{id_}" does not exist')
+
+    return {"version": doc.version}
