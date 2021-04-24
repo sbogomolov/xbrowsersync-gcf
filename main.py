4abbbe4b8e13e456d89cbbb607d33dcf0d28b7d3
Sergii Bogomolov <sergii@bogomolov.io>
2021-04-24 02:49:21 +0200

    Add support for ACCEPT_NEW_SYNCS

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import os
+
 def info(request):
+    accept_new_syncs = os.environ.get("ACCEPT_NEW_SYNCS", "false")
+    status = 1 if accept_new_syncs.lower() == "true" else 3
     return {
         "location": "",
         "maxSyncSize": 512000,
         "message": "",
-        "status": 3,
+        "status": status,
         "version": "1.1.13",
     }
